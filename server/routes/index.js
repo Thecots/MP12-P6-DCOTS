40fd5d2e99ea76301e8db461a99d09596badfa30
Thecots <thecots22@gmail.com>
2021-11-05 12:37:49 +0100

    xd

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get('/',(req,res)=> {
-    res.send('<a href="/user">Click</a>')
+    res.send('<a href="/user">Usuarios</a>')
 })
 app.use(require("./users"));
 
