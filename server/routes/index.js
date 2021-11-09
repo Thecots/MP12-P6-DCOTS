87dc1c694a34977894c3938479e1d797e8cf7fc8
Thecots <dcots@insdanielblanxart.cat>
2021-11-09 18:55:52 +0100

    si

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,8 @@ const app = express();
 app.get('/',(req,res)=> {
     res.send('<a href="/user">Usuarios</a>')
 })
+
 app.use(require("./users"));
+app.use(require("./clients"));
 
 module.exports = app;
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,67 +3,60 @@ const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const app = express();
 
+/* get */
 app.get("/user", (req, res) => {
-    User.find({}, "username email").exec((err, usuaris) => {
-      if (err) {
-        return res.status(400).json({
-          ok: false,
-          err,
-        });
-      }
-  
-      res.json({
-        ok: true,
-        usuaris,
+  User.find().exec((err, usuaris) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        err,
       });
-    });
-  });
-  
-  app.post("/user", (req, res) => {
-    let body = req.body;
-    let user = new User({
-      username: body.username,
-      email: body.email,
-      password: bcrypt.hashSync(body.password, 10),
-    });
-  
-
-  
-    user.save((err, userDB) => {
-      if (err) {
-        return res.status(400).json({
-          ok: false,
-          err,
-        });
-      }
-  
-      res.json({
-        ok: true,
-        user: userDB,
-      });
-    });
-  });
-  
-  app.put("/user",async(req,res) => {
-    const {username, email, password} = req.body
-
-    const user = new User({username,email,password});
-      
-    user.save((err,userDB) => {
-
-    })
-
-      
-  })
-
-  app.delete("/user",(req,res) => {
+    }
     res.json({
       ok: true,
-      name,
-      email,
-      password
+      usuaris,
+    });
+  });
+});
+  
+/* put */
+app.put("/user", (req, res) => {
+  let body = req.body;
+  let user = new User({
+    username: body.username,
+    email: body.email,
+    password: bcrypt.hashSync(body.password, 10),
+  });
 
-    })
-  })
+  user.save((err, userDB) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        err,
+      });
+    }
+    res.json({
+      ok: true,
+      user: userDB,
+    });
+  });
+});
+
+
+/* delete */
+app.delete("/user",(req,res) => {
+  User.deleteOne({_id: req.body.id}, (err) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        err,
+      });
+    };
+
+    res.json({
+      ok: true,
+    });
+  });
+})
   module.exports = app;
   
